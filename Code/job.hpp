7badfdae95584e2a457efb18e67c5c6c0167f18d
Pravin Ramana <pravinramana25@protonmail.ch>
2023-09-23 10:53:33 -0500

    Update Job implementation

--- Code/job.hpp
+++ Code/job.hpp
@@ -1,17 +1,18 @@
+#ifndef JOB_HPP
+#define JOB_HPP
+
 /// The Job interface for which the Job System will execute jobs
 class Job {
-private:
+public:
   /// A unique identifier for the job
   int id;
-  /// An optional type for the job
-  int type;
-  /// The channels that this job may run on
-  unsigned long channels;
 
-public:
   /// Execute() function, which is ran on Slave threads. This is run first
   virtual void Execute() = 0;
 
-  /// A callback function that is called after Execute() completes on the Master thread. This function should focus on cleanup.
-  virtual void JobCompleteCallback() {};
+  /// A callback function that is called after Execute() completes on the Master
+  /// thread. This function should focus on cleanup/lightweight tasks.
+  virtual void JobCompleteCallback(){};
 };
+
+#endif
