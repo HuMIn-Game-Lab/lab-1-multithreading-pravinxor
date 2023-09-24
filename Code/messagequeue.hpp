767c84e2ec0154569c7ac3e48b32fdb926cd4866
Pravin Ramana <pravinramana25@protonmail.ch>
2023-09-24 10:59:38 -0500

    Rework message queue to support regular queue and priority queue specializations (for master and slave messages, respectively)

--- Code/messagequeue.hpp
+++ Code/messagequeue.hpp
@@ -3,7 +3,8 @@
 #include <queue>
 #include <vector>
 
-template <typename T> class MessageQueue {
+template <typename T, template <typename...> class Container, typename C = void>
+class MessageQueue {
 private:
   /// Lock to manage synchronization between threads internal queue
   std::mutex messages_mtx;
@@ -12,7 +13,8 @@ private:
   /// message available
   std::condition_variable cv;
 
-  std::queue<T> messages;
+  /// Specializable container, which stores the messages
+  Container<T> messages;
 
 public:
   /// Tests whether the queue is empty
@@ -22,43 +24,89 @@ public:
   }
 
   /// Adds a message to the queue
-  void send(const T &val);
-
+  void send(const T &val) {
+    std::unique_lock<std::mutex> messages_lock(this->messages_mtx);
+    this->messages.push(val);
+    messages_lock.unlock();
+    this->cv.notify_one();
+  }
   /// Receives the next message in the queue. Note: this blocks execution until
   /// a message is available
-  T receive();
+  T receive() {
+    std::unique_lock<std::mutex> messages_lock(this->messages_mtx);
+    cv.wait(messages_lock, [this] { return !this->messages.empty(); });
 
-  /// Recieves a specific amount of messages in one call
-  std::vector<T> receive_n(unsigned long n);
-};
-
-template <typename T> void MessageQueue<T>::send(const T &val) {
-  std::unique_lock<std::mutex> messages_lock(this->messages_mtx);
-  this->messages.push(val);
-  messages_lock.unlock();
-  this->cv.notify_one(); // Notify any waiting recievers of available element
-}
-
-template <typename T> T MessageQueue<T>::receive() {
-  std::unique_lock<std::mutex> messages_lock(this->messages_mtx);
-  cv.wait(messages_lock, [this] { return !this->messages.empty(); });
-
-  T val = messages.front();
-  messages.pop();
-  return val;
-}
-
-template <typename T>
-std::vector<T> MessageQueue<T>::receive_n(unsigned long n) {
-  std::unique_lock<std::mutex> messages_lock(this->messages_mtx);
-  cv.wait(messages_lock, [this, n] { return this->messages.size() >= n; });
-
-  std::vector<T> flushed_messages = std::vector<T>(n);
-
-  for (unsigned long k = 0; k < n; ++k) {
-    flushed_messages.push_back(this->messages.front());
-    this->messages.pop();
+    T val = pop_impl(this->messages);
+    return val;
   }
 
-  return flushed_messages;
+  /// Recieves a specific amount of messages in one call. Note: blocks execution
+  /// until n messages are available
+  std::vector<T> receive_n(unsigned long n) {
+    std::unique_lock<std::mutex> messages_lock(this->messages_mtx);
+    cv.wait(messages_lock, [this, n] { return this->messages.size() >= n; });
+
+    std::vector<T> flushed_messages(n);
+
+    for (unsigned long k = 0; k < n; ++k) {
+      flushed_messages.push_back(pop_impl(this->messages));
+    }
+
+    return flushed_messages;
+  }
+};
+
+template <typename T> T pop_impl(std::queue<T> &c) {
+  T value = c.front();
+  c.pop();
+  return value;
+}
+
+template <typename T, typename C>
+class MessageQueue<T, std::priority_queue, C> {
+private:
+  std::mutex messages_mtx;
+  std::condition_variable cv;
+  std::priority_queue<T, std::vector<T>, C> messages;
+
+public:
+  bool empty() {
+    std::lock_guard<std::mutex> messages_lock(this->messages_mtx);
+    return this->messages.empty();
+  }
+
+  void send(const T &val) {
+    std::unique_lock<std::mutex> messages_lock(this->messages_mtx);
+    this->messages.push(val);
+    messages_lock.unlock();
+    this->cv.notify_one(); // Notify any waiting recievers of available element
+  }
+
+  T receive() {
+    std::unique_lock<std::mutex> messages_lock(this->messages_mtx);
+    cv.wait(messages_lock, [this] { return !this->messages.empty(); });
+
+    T val = pop_impl(this->messages);
+    return val;
+  }
+
+  std::vector<T> receive_n(unsigned long n) {
+    std::unique_lock<std::mutex> messages_lock(this->messages_mtx);
+    cv.wait(messages_lock, [this, n] { return this->messages.size() >= n; });
+
+    std::vector<T> flushed_messages;
+
+    for (unsigned long k = 0; k < n; ++k) {
+      flushed_messages.push_back(pop_impl(this->messages));
+    }
+
+    return flushed_messages;
+  }
+};
+
+template <typename T, typename C>
+T pop_impl(std::priority_queue<T, std::vector<T>, C> &c) {
+  T value = c.top();
+  c.pop();
+  return value;
 }
