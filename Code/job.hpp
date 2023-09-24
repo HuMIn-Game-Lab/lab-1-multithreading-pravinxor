166ba562d8c921c3df1f3bda3f92dd4ca6330f8d
Pravin Ramana <pravinramana25@protonmail.ch>
2023-09-24 11:01:03 -0500

    Replace job callback with chain_next(), which allows jobs to easily chain new jobs to the system without needing to return to that master thread

--- Code/job.hpp
+++ Code/job.hpp
@@ -1,18 +1,26 @@
 #ifndef JOB_HPP
 #define JOB_HPP
 
+#include <atomic>
+
+class JobSystem;
+
 /// The Job interface for which the Job System will execute jobs
 class Job {
+
 public:
+
   /// A unique identifier for the job
-  int id;
+  unsigned int id;
 
   /// Execute() function, which is ran on Slave threads. This is run first
-  virtual void Execute() = 0;
+  virtual void execute() = 0;
 
-  /// A callback function that is called after Execute() completes on the Master
-  /// thread. This function should focus on cleanup/lightweight tasks.
-  virtual void JobCompleteCallback(){};
+  /// If a job has dependencies, it may return another job to be immediately
+  /// executed (if possible). Notice!!!: Overloading this will likely require
+  /// dynamic memory allocation, which should be freed once removed
+  /// This is called as soon as execute) is finished (from the Slave thread)
+  virtual void chain_next(JobSystem *system){};
 
   virtual ~Job(){};
 };
