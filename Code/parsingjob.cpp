43511a97378305874fc170b9a618cc2339836fbf
Pravin Ramana <pravinramana25@protonmail.ch>
2023-09-28 11:50:26 -0500

    Support parsing linker errors

--- Code/parsingjob.cpp
+++ Code/parsingjob.cpp
@@ -7,18 +7,22 @@
 #include <regex>
 #include <sstream>
 #include <string>
+#include <vector>
 
 ParsingJob::ParsingJob(unsigned int id, std::string ingest) : ingest(ingest) {
   this->id = id;
 }
 
 void ParsingJob::execute() {
+  std::regex linker_txt_expr(
+      "\\(.\\w+\\+0x\\w+\\): undefined reference to `\\w+'");
   std::regex linker_expr("clang-\\d+: error: (.*)");
   std::regex compiler_expr("(.*):(\\d+):(\\d+): (?:error|warning): (.*)");
 
   std::istringstream ess(this->ingest);
 
   std::string line;
+  std::string linker_errs;
   while (std::getline(ess, line)) {
     std::smatch match;
     if (std::regex_match(line, match, compiler_expr)) {
@@ -39,6 +43,23 @@ void ParsingJob::execute() {
         }
         error_vec->second.push_back(error);
       }
+    }
+    if (std::regex_match(line, match, linker_txt_expr)) {
+      linker_errs.append(line + '\n');
+    }
+    if (std::regex_match(line, match, linker_expr)) {
+      Error error{
+          .line = 0,
+          .column = 0,
+          .message = linker_errs,
+      };
+      std::string filename = "/dev/null";
+      auto error_vec = this->errors.find(filename);
+      if (error_vec == this->errors.end()) {
+        this->errors.insert({filename, {}});
+        error_vec = this->errors.find(filename);
+      }
+      error_vec->second.push_back(error);
     }
   }
 }
