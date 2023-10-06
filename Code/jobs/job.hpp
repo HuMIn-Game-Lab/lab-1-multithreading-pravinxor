9560f862654a2aec4f932d62df1e6c7b3c137a81
Pravin Ramana <pravinramana25@protonmail.ch>
2023-10-06 16:32:47 -0500

    Update job interface to transition to new architecture

--- Code/jobs/job.hpp
+++ Code/jobs/job.hpp
@@ -5,23 +5,12 @@
 
 class JobSystem;
 
-/// The Job interface for which the Job System will execute jobs
-class Job {
+/// A descriptor for a job to be executed
+template <typename T> class Job {
 
 public:
-  /// A unique identifier for the job
-  unsigned int id;
-
   /// Execute() function, which is ran on Slave threads. This is run first
-  virtual void execute() = 0;
-
-  /// If a job has dependencies, it may return another job to be immediately
-  /// executed (if possible). Notice!!!: Overloading this will likely require
-  /// dynamic memory allocation, which should be freed once removed
-  /// This is called as soon as execute) is finished (from the Slave thread)
-  virtual void chain_next(JobSystem *system){};
-
-  virtual ~Job(){};
+  virtual T execute() = 0;
 };
 
 #endif
